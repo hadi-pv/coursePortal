d11369482d2db8655aa06c0b7df7cb510a85eac0
hadi <chumma1357@gmail.com>
2023-07-25 11:42:09 +0530

    started

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthComponent } from './auth/auth.component';
+import { UserComponent } from './user/user.component';
+import { AdminComponent } from './admin/admin.component';
+import { SidenavComponent } from './shared/components/sidenav/sidenav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent,
+    UserComponent,
+    AdminComponent,
+    SidenavComponent
   ],
   imports: [
     BrowserModule,
